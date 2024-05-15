895dd6d09e30b2778b62f89aae935d275a12511f
Nahuelgonzalez7 <nahuelgonzalez@hotmail.es>
2024-05-14 22:22:08 -0300

    created common folder to move header and footer views, added methods to load header and footer dynamically

--- src/views/contact/js/contact.js
+++ src/views/contact/js/contact.js
@@ -1,5 +1,7 @@
 import { loadHeader } from "../../../common/header/header.js"
+import { loadFooter } from "../../../common/footer/footer.js"
 
 // Use case
 var actualView = '../contact/contact.html'; // Replace with the path of the actual view.
 loadHeader(actualView);
+loadFooter();
