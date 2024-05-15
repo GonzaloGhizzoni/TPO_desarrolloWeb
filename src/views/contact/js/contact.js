7bc25626d63fbd7aa6a9ee70ee79acc20737969c
Nahuelgonzalez7 <nahuelgonzalez@hotmail.es>
2024-05-14 22:02:07 -0300

    added contact script reference, deleted header reference and added form fields

---
+++ src/views/contact/js/contact.js
@@ -0,0 +1,5 @@
+import { loadHeader } from "../../../common/header/header.js"
+
+// Use case
+var actualView = '../contact/contact.html'; // Replace with the path of the actual view.
+loadHeader(actualView);
