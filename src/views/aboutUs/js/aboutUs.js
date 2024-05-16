a2b5b227e124f860d4f5d2524082060447c85d51
GonzaloGhizzoni <gonzaghizzoni@gmail.com>
2024-05-16 16:14:55 -0300

    added bootstrap navbar

---
+++ src/views/aboutUs/js/aboutUs.js
@@ -0,0 +1,7 @@
+import { loadHeader } from "../../../common/header/header.js"
+import { loadFooter } from "../../../common/footer/footer.js"
+
+// Use case
+var actualView = '../aboutUs.html'; // Replace with the path of the actual view.
+loadHeader(actualView);
+loadFooter();
