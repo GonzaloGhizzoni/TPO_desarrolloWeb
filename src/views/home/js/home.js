bf838037171def867dc4ec02a588ff66959c0550
santiagochapa-dev <santichapa2003@gmail.com>
2024-05-17 23:54:43 -0300

    added-footer-header

---
+++ src/views/home/js/home.js
@@ -0,0 +1,7 @@
+import { loadHeader } from "../../../common/header/header.js"
+import { loadFooter } from "../../../common/footer/footer.js"
+
+// Use case
+var actualView = "../home/home.html"; // Replace with the path of the actual view.
+loadHeader(actualView);
+loadFooter();
