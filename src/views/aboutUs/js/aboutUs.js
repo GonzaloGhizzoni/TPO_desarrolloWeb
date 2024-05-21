0c096a6a3ecdde52bb62cdd2e54b3d48b4bf002a
Nahuelgonzalez7 <nahuelgonzalez@hotmail.es>
2024-05-20 23:44:17 -0300

    deleted console.log

--- src/views/aboutUs/js/aboutUs.js
+++ src/views/aboutUs/js/aboutUs.js
@@ -3,7 +3,6 @@ import { loadFooter } from "../../../common/footer/footer.js"
 
 // Use case
 var actualView = '../aboutUs/aboutUs.html'; // Replace with the path of the actual view. ACTUAL VIEW MUST MATCH WITH HEADEAR.HTML HREF PATH
-console.log(actualView);
 
 loadHeader(actualView);
 loadFooter();
