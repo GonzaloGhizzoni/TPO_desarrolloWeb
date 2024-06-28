2f2e7582c0b656b5e20bcb1ac06ccbd7130e38e0
NahuelGonzalez7 <nahuelgonzalez07@hotmail.es>
2024-06-28 20:47:18 -0300

    added login method and call to API

--- src/views/login/js/login.js
+++ src/views/login/js/login.js
@@ -1,6 +1,8 @@
 import { loadHeader } from "../../../common/header/header.js"
 import { loadFooter } from "../../../common/footer/footer.js"
 
+const apiUrl = "http://127.0.0.1:5000"
+
 // Use case
 var actualView = '../login/login.html'; // Replace with the path of the actual view. ACTUAL VIEW MUST MATCH WITH HEADEAR.HTML HREF PATH
 
@@ -19,3 +21,36 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        
+        let email = document.getElementById('fEmail').value;
+        let password = document.getElementById('fPassword').value;
+        
+        // Call login function
+        login(email, password);
+    });
+});
+
+
+function login(email, password) {
+    fetch(`${apiUrl}/login/${encodeURIComponent(email)}/${encodeURIComponent(password)}`, {
+        method: "GET"
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+
+    console.log(email);
+    console.log(password);
+}
