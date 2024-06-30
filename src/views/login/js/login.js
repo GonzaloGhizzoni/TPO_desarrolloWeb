83fad9a0ea9de9d73b4e5fc7609296b4f685a643
NahuelGonzalez7 <nahuelgonzalez07@hotmail.es>
2024-06-30 16:47:30 -0300

    added validate form method and clear form fields method

--- src/views/login/js/login.js
+++ src/views/login/js/login.js
@@ -17,38 +17,67 @@ const loginBtn = document.getElementById('login');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
+    clearFormFields();
 });
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
+    clearFormFields();
 });
 
 document.addEventListener('DOMContentLoaded', function() {
     const loginForm = document.getElementById('loginForm');
+    const inputs = document.querySelectorAll('input');
+
+
     loginForm.addEventListener('submit', function(event) {
-        event.preventDefault(); 
+        let formIsValid = true;
         
+        inputs.forEach(input => {
+            if (!validateField(loginForm,input)) {
+                formIsValid = false;
+            }
+        });
+
+        if (!formIsValid)
+            event.preventDefault(); 
+
         let email = document.getElementById('fEmail').value;
         let password = document.getElementById('fPassword').value;
         
         // Call login function
         login(email, password);
+        clearFormFields();
     });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    const loginForm = document.getElementById('registerForm');
-    loginForm.addEventListener('submit', function(event) {
-        event.preventDefault(); 
-        
-        let email = document.getElementById('FregisterEmail').value;
-        let password = document.getElementById('FregisterPassword').value;
-        let name = document.getElementById('fName').value;
-        let surname = document.getElementById('fSurname').value;
-        let birthdate = document.getElementById('fbirthdate').value;
-        
-        // Call login function
-        register(email,name,surname,birthdate,password);
+    const registerForm = document.getElementById('registerForm');
+    const inputs = document.querySelectorAll('input');
+
+    registerForm.addEventListener('submit', function(event) {
+        let formIsValid = true;
+
+        inputs.forEach(input => {
+            if (!validateField(registerForm,input)) {
+                formIsValid = false;
+            }
+        });
+
+
+        if (!formIsValid)
+            event.preventDefault(); 
+        else{
+            let email = document.getElementById('FregisterEmail').value;
+            let password = document.getElementById('FregisterPassword').value;
+            let name = document.getElementById('fName').value;
+            let surname = document.getElementById('fSurname').value;
+            let birthdate = document.getElementById('fbirthdate').value;
+            
+            // Call login function
+            register(email,name,surname,birthdate,password);
+            clearFormFields();
+        }
     });
 });
 
@@ -64,6 +93,7 @@ function login(email, password) {
     .then(response => {
         console.log(response);
         if (!response.ok) {
+            alert("Invalid email or password")
             return response.json()
         }
         return response.json();
@@ -92,4 +122,36 @@ function register(email,name,surname,birthdate,password){
         console.log(data);
     })
     .catch(error => console.error('Error:', error));
+}
+
+function clearFormFields() {
+    const loginForm = document.getElementById('loginForm');
+    const registerForm = document.getElementById('registerForm');
+
+    // Clear login form fields
+    loginForm.querySelectorAll('input[type="email"], input[type="password"]').forEach(input => {
+        input.value = '';
+    });
+
+    // Clear register form fields
+    registerForm.querySelectorAll('input[type="text"], input[type="password"], input[type="email"], input[type="date"]').forEach(input => {
+        input.value = '';
+    });
+}
+
+function validateField(form,field) {
+    const fieldType = field.getAttribute('type');
+    const fieldValue = field.value.trim();
+
+    if (fieldType === 'text' || fieldType === 'email' || fieldType === 'date' || fieldType === 'password') {
+        if (fieldValue.length === 0) {
+            field.classList.add('is-invalid');
+            field.nextElementSibling.textContent = 'Debe completar este campo.';
+            return false;
+        } else {
+            field.classList.remove('is-invalid');
+            field.nextElementSibling.textContent = '';
+            return true;
+        }
+    }
 }
