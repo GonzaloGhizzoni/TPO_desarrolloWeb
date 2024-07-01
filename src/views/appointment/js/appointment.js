e4995c2927144dc87d48a4976ae127ab41f70e83
NahuelGonzalez7 <nahuelgonzalez07@hotmail.es>
2024-06-30 23:47:59 -0300

    added apiURL and add appointment method

--- src/views/appointment/js/appointment.js
+++ src/views/appointment/js/appointment.js
@@ -1,6 +1,10 @@
 import { loadHeader } from "../../../common/header/header.js"
 import { loadFooter } from "../../../common/footer/footer.js"
 
+
+const apiUrl = "http://127.0.0.1:5000"
+// const apiUrl = "https://nahuelgr.pythonanywhere.com"
+
 // Header and footer loading function
 // -------------------------------------------------------- //
 // Use case
@@ -10,15 +14,16 @@ loadFooter();
 
 
 /////////////////////////////////////////////////
-
+// Function to load hour's selector
 document.addEventListener('DOMContentLoaded', () => {
+    getUserAppointments(1);
     // Bloquear fechas pasadas en el selector de fecha
-    const dateInput = document.getElementById('date');
+    const dateInput = document.getElementById('fDate');
     const today = new Date().toISOString().split('T')[0];
     dateInput.setAttribute('min', today);
 
     // Generar opciones de horario
-    const timeSelect = document.getElementById('time');
+    const timeSelect = document.getElementById('fTime');
     const startHour = 9;
     const endHour = 18;
     const interval = 30;
@@ -33,3 +38,94 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+
+/////////////////////////////////////////////////
+
+document.addEventListener('DOMContentLoaded', function() {
+    const bookingForm = document.getElementById('booking-form');
+    const inputs = document.querySelectorAll('input');
+
+    bookingForm.addEventListener('submit', function(event) {
+        let formIsValid = true;
+
+        // inputs.forEach(input => {
+        //     if (!validateField(bookingForm,input)) {
+        //         formIsValid = false;
+        //     }
+        // });
+
+
+        // if (!formIsValid)
+        //     event.preventDefault(); 
+        // else{
+            let email = document.getElementById('fEmail').value;
+            let name = document.getElementById('fName').value;
+            let specialties = document.getElementById('fSpecialty').value;
+            let dateOfAppointment = document.getElementById('fDate').value;
+            let hour = document.getElementById('fTime').value;
+            
+            console.log(email);
+            console.log(name);
+            console.log(specialties);
+            console.log(dateOfAppointment);
+            console.log(hour);
+
+
+            // Call login function
+            addAppointment(email,name,specialties,dateOfAppointment,hour);
+            // clearFormFields();
+        // }
+    });
+});
+
+
+function addAppointment(name, email, specialties, dateOfAppointment, hour) {
+    if (!navigator.onLine) {
+        // Display error message: "Unable to connect to server. Check your internet connection."
+        return console.log("error al conectar al server");
+    }
+    fetch(`${apiUrl}/newappointment`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: name, email: email, specialties: specialties,dateOfAppointment: dateOfAppointment,hour: hour })
+    })
+    .then(response => {
+        console.log(response);
+        if (!response.ok) {
+            console.log("Entro aca")
+            return response.json()
+        }
+        return response.json()
+    })
+    .then(data => {
+        console.log("Entro al data")
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+function getUserAppointments(userID){
+    fetch(`${apiUrl}/userappointment`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ userID: userID})
+    })
+    .then(response => {
+        console.log(response);
+        if (!response.ok) {
+            console.log("Entro aca")
+            return response.json()
+        }
+        return response.json()
+    })
+    .then(data => {
+        console.log("Entro al data")
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+
+}
